02b6e173e51ef5be5258d4e5a6b566f93c9fc5bd
virgostyx <virgostyx@gmail.com>
2026-07-31 14:40:16 +0200

    Add "My account" link to sidebar above "Sign out" with tests

--- spec/components/entities/sidebar_component_spec.rb
+++ spec/components/entities/sidebar_component_spec.rb
@@ -449,6 +449,13 @@ RSpec.describe Entities::SidebarComponent, type: :component do
     it "renders sign out as a link with turbo_method delete" do
       expect(rendered).to have_css("a[data-turbo-method='delete']", text: "Sign out")
     end
+
+    it "links to the account profile page, above sign out" do
+      expect(rendered).to have_link("My account", href: edit_user_registration_path)
+
+      links = rendered.css("a").map { |a| a.text.squish }
+      expect(links.index("My account")).to be < links.index("Sign out")
+    end
   end
 
   describe "department badge" do
